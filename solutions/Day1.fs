37c32817a3b748d34c1b477bd74ea356f72bacf4
Chase Manning <chase@manning.dev>
2023-12-01 19:26:06 +0000

    :rocket: add day 1 part 2 solution

--- solutions/Day1.fs
+++ solutions/Day1.fs
@@ -1,13 +1,23 @@
 module Day1
 
-let solvePartOne (input: string[]) =
-    let isDigit c = System.Char.IsDigit c
-    let toInt c = System.Int32.Parse(c.ToString())
+let numbers =
+    [ "one"; "two"; "three"; "four"; "five"; "six"; "seven"; "eight"; "nine" ]
 
+let isDigit c = System.Char.IsDigit c
+let toInt c = System.Int32.Parse(c.ToString())
+let combine (a: int) (b: int) = a * 10 + b
+
+let isNumber (s: string) =
+    numbers |> List.exists (fun num -> s.EndsWith(num))
+
+let toNumber (s: string) =
+    (numbers |> List.findIndex (fun num -> s.EndsWith(num))) + 1
+
+let getSum (input: string[]) (includeText: bool) =
     let mutable sum = 0
 
     for line in input do
-
+        let mutable text = ""
         let mutable first = -1
         let mutable last = 0
 
@@ -19,11 +29,20 @@ let solvePartOne (input: string[]) =
                     first <- toInt c
 
                 last <- toInt c
+                text <- ""
+            else if includeText then
+                text <- text + c.ToString()
 
-        let number = first * 10 + last
-        sum <- sum + number
+                if isNumber text then
+                    if first = -1 then
+                        first <- toNumber text
+
+                    last <- toNumber text
+
+        sum <- sum + combine first last
 
     sum.ToString()
 
+let solvePartOne (input: string[]) = getSum input false
 
-let solvePartTwo (input: string[]) = "TODO"
+let solvePartTwo (input: string[]) = getSum input true
