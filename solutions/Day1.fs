9ac9d586caa72bd7bbefd915da24f709c87419eb
Chase Manning <chase@manning.dev>
2023-12-01 18:51:11 +0000

    :rocket: add day 1 part 1 solution

--- solutions/Day1.fs
+++ solutions/Day1.fs
@@ -1,5 +1,29 @@
 module Day1
 
-let solvePartOne (word: string) = "TODO"
+let solvePartOne (input: string[]) =
+    let isDigit c = System.Char.IsDigit c
+    let toInt c = System.Int32.Parse(c.ToString())
 
-let solvePartTwo (word: string) = "TODO"
+    let mutable sum = 0
+
+    for line in input do
+
+        let mutable first = -1
+        let mutable last = 0
+
+        for i = 0 to line.Length - 1 do
+            let c = line.[i]
+
+            if isDigit c then
+                if first = -1 then
+                    first <- toInt c
+
+                last <- toInt c
+
+        let number = first * 10 + last
+        sum <- sum + number
+
+    sum.ToString()
+
+
+let solvePartTwo (input: string[]) = "TODO"
--- solutions/Day2.fs
+++ solutions/Day2.fs
@@ -1,5 +1,5 @@
 module Day2
 
-let solvePartOne (word: string) = "TODO"
+let solvePartOne (input: string[]) = "TODO"
 
-let solvePartTwo (word: string) = "TODO"
+let solvePartTwo (input: string[]) = "TODO"
