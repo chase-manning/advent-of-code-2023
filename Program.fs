e62be242ace31a046a8f05808e6bd34a0e173642
Chase Manning <chase@manning.dev>
2023-12-01 18:25:02 +0000

    :rocket: add cli for running solutions

--- Program.fs
+++ Program.fs
@@ -1,22 +1,24 @@
-﻿open System
-
-module PigLatin =
-    let toPigLatin (word: string) =
-        let isVowel (c: char) =
-            match c with
-            | 'a' | 'e' | 'i' |'o' |'u'
-            | 'A' | 'E' | 'I' | 'O' | 'U' -> true
-            |_ -> false
-        
-        if isVowel word[0] then
-            word + "yay"
-        else
-            word[1..] + string word[0] + "ay"
-
-[<EntryPoint>]
+﻿[<EntryPoint>]
 let main args =
-    for arg in args do
-        let newArg = PigLatin.toPigLatin arg
-        printfn "%s in Pig Latin is: %s" arg newArg
+    // Get the day and part from the command line arguments
+    let day = args.[0]
+    let part = args.[1]
 
+    // Run the solution
+    let result =
+        match day with
+        | "1" ->
+            match part with
+            | "1" -> Day1.solvePartOne part
+            | "2" -> Day1.solvePartTwo part
+            | _ -> "Unknown part"
+        | "2" ->
+            match part with
+            | "1" -> Day2.solvePartOne part
+            | "2" -> Day2.solvePartTwo part
+            | _ -> "Unknown part"
+        | _ -> "Unknown day"
+
+    // Print the result
+    printfn "%s" result
     0
