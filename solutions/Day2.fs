66f51f5e87efe99ee02996c48b404a58213890eb
Chase Manning <chase@manning.dev>
2023-12-02 11:50:33 +0000

    :art: minor code refactoring

--- solutions/Day2.fs
+++ solutions/Day2.fs
@@ -1,27 +1,24 @@
 module Day2
 
-let RED = 12
-let GREEN = 13
-let BLUE = 14
+open System
 
 type Round = { red: int; green: int; blue: int }
-
 type Game = { Id: int; Rounds: Round list }
 
-let get_id (line: string) : int =
+let get_id (line: string) =
     let id = line.Split(' ')[1]
-    System.Int32.Parse(id.[.. (id.Length - 2)])
+    Int32.Parse(id.[.. (id.Length - 2)])
 
-let is_color (color: string) (pick: string) : bool = pick.EndsWith(color)
+let is_color (color: string) (pick: string) = pick.EndsWith(color)
 
-let color_amount (pick: string) : int = System.Int32.Parse(pick.Split(' ')[0])
+let color_amount (pick: string) = Int32.Parse(pick.Split(' ')[0])
 
-let get_count picks color : int =
+let get_count picks color =
     picks
     |> List.filter (is_color color)
     |> List.fold (fun acc pick -> acc + color_amount pick) 0
 
-let get_round (text: string) : Round =
+let get_round (text: string) =
     let picks = text.Split(", ") |> Array.toList
 
     { red = get_count picks "red"
@@ -29,34 +26,34 @@ let get_round (text: string) : Round =
       blue = get_count picks "blue" }
 
 
-let get_rounds (line: string) : Round List =
+let get_rounds (line: string) =
     (line.Split(": ")[1]).Split("; ") |> Array.toList |> List.map get_round
 
-let get_game line : Game =
+let get_game line =
     { Id = get_id line
       Rounds = get_rounds line }
 
-let get_games lines : Game List = List.map get_game lines
+let get_games lines = List.map get_game lines
 
-let is_round_possible round : bool =
-    round.red <= RED && round.green <= GREEN && round.blue <= BLUE
+let is_round_possible round =
+    round.red <= 12 && round.green <= 13 && round.blue <= 14
 
-let is_game_possible game : bool =
+let is_game_possible game =
     List.forall is_round_possible game.Rounds
 
-let solvePartOne (input: string List) : string =
+let solvePartOne input =
     get_games input
     |> List.filter is_game_possible
     |> List.fold (fun acc game -> acc + game.Id) 0
     |> string
 
-let max_colors game : Round =
+let max_colors game =
     game.Rounds
     |> List.fold
         (fun acc round ->
-            { red = System.Math.Max(acc.red, round.red)
-              green = System.Math.Max(acc.green, round.green)
-              blue = System.Math.Max(acc.blue, round.blue) })
+            { red = Math.Max(acc.red, round.red)
+              green = Math.Max(acc.green, round.green)
+              blue = Math.Max(acc.blue, round.blue) })
         { red = 0; green = 0; blue = 0 }
 
 let power (round: Round) : int = round.red * round.green * round.blue
