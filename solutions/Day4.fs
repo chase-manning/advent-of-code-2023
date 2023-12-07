a3b4083c03fcf80eae28ea788ad7d238d1aac154
Chase Manning <chase@manning.dev>
2023-12-07 22:56:08 +0000

    :rocket: add day 4 part 2

--- solutions/Day4.fs
+++ solutions/Day4.fs
@@ -12,13 +12,13 @@ let lineParser (line: string) =
     let sides = numberStrings.Split(" | ")
     (numbers sides[0], numbers sides[1])
 
-let points winning selection =
-    let correct =
-        selection
-        |> List.filter (fun x -> winning |> List.exists (fun y -> x = y))
-        |> List.length
-        |> float
+let correct (winning: int List) (selection: int List) =
+    selection
+    |> List.filter (fun x -> winning |> List.exists (fun y -> x = y))
+    |> List.length
 
+let points winning selection =
+    let correct = correct winning selection |> float
     if correct = 0 then 0 else (2.0 ** (correct - 1.0)) |> int32
 
 let part1 (input: string List) =
@@ -27,4 +27,31 @@ let part1 (input: string List) =
     |> List.fold (fun acc (winning, selection) -> acc + points winning selection) 0
     |> string
 
-let part2 (input: string List) = "meow"
+let totalCards (points: int List) : int =
+    let rec run (cards: int List) pos : int =
+        let rec addCards (cards_: int List) (pos: int) (toAdd: int) (scale: int) : int List =
+            if toAdd = 0 then
+                cards_
+            else if pos = cards_.Length then
+                cards_
+            else
+                let newCards =
+                    cards_ |> List.mapi (fun i card -> if i = pos then card + scale else card)
+
+                addCards newCards (pos + 1) (toAdd - 1) scale
+
+        if pos = points.Length then
+            cards |> List.sum
+        else
+            let newCards = addCards cards (pos + 1) points.[pos] cards.[pos]
+            run newCards (pos + 1)
+
+    run (points |> List.map (fun _ -> 1)) 0
+
+let part2 (input: string List) =
+    let cards = input |> List.map lineParser
+
+    let points =
+        cards |> List.map (fun (winning, selection) -> correct winning selection)
+
+    totalCards points |> string
