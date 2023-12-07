f03ed0c63a9f693eac9376bd314f25b5647d375a
Chase Manning <chase@manning.dev>
2023-12-07 22:22:27 +0000

    :rocket: add day 4 part 1 solution

--- solutions/Day4.fs
+++ solutions/Day4.fs
@@ -2,6 +2,29 @@ module Day4
 
 open System
 
-let part1 (input: string List) = "meow"
+let toNumber s = Int32.Parse(s.ToString())
+
+let numbers (s: string) =
+    s.Split(' ') |> Array.toList |> List.map toNumber
+
+let lineParser (line: string) =
+    let numberStrings = line.Replace("  ", " ").Split(": ")[1]
+    let sides = numberStrings.Split(" | ")
+    (numbers sides[0], numbers sides[1])
+
+let points winning selection =
+    let correct =
+        selection
+        |> List.filter (fun x -> winning |> List.exists (fun y -> x = y))
+        |> List.length
+        |> float
+
+    if correct = 0 then 0 else (2.0 ** (correct - 1.0)) |> int32
+
+let part1 (input: string List) =
+    input
+    |> List.map lineParser
+    |> List.fold (fun acc (winning, selection) -> acc + points winning selection) 0
+    |> string
 
 let part2 (input: string List) = "meow"
