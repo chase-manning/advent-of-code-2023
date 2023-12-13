0fd1cff1c322b9544e86821b91b209eedc6b78e0
Chase Manning <chase@manning.dev>
2023-12-13 20:55:39 +0000

    add day 9 part 2 solution

--- solutions/Day9.fs
+++ solutions/Day9.fs
@@ -20,10 +20,31 @@ let getPrediction (reports: int List) : int =
     let lastNumbers = run reports []
     lastNumbers |> List.sum
 
+let getStartPrediction (reports: int List) : int =
+    let rec run (difference: int List) (firstNumbers: int List) =
+        if difference |> List.forall (fun i -> i = 0) then
+            firstNumbers
+        else
+            run (getDifference difference) (firstNumbers @ [ difference[0] ])
+
+    let lastNumbers = run reports []
+
+    let rec runReduce (index: int) (total: int) =
+        if index = -1 then
+            total
+        else
+            runReduce (index - 1) (lastNumbers[index] - total)
+
+    runReduce (lastNumbers.Length - 1) 0
+
+
 
 let part1 (input: string List) =
     let reports = getReports input
     let predictions = reports |> List.map getPrediction
     predictions |> List.sum |> string
 
-let part2 (input: string List) = "meow"
+let part2 (input: string List) =
+    let reports = getReports input
+    let predictions = reports |> List.map getStartPrediction
+    predictions |> List.sum |> string
