cc81086247cd4812269376727fd7728709feb246
Chase Manning <chase@manning.dev>
2023-12-13 20:57:52 +0000

    minor code improvements

--- solutions/Day9.fs
+++ solutions/Day9.fs
@@ -1,27 +1,24 @@
 module Day9
 
-open System
-
-let getReport (input: string) : int List =
+let getReport (input: string) =
     input.Split(" ") |> Array.toList |> List.map int
 
-let getReports (input: string List) : (int List) List = input |> List.map getReport
+let getReports input = input |> List.map getReport
 
-let getDifference (reports: int List) : int List =
+let getDifference (reports: int List) =
     [ 1 .. reports.Length - 1 ] |> List.map (fun i -> reports.[i] - reports.[i - 1])
 
-let getPrediction (reports: int List) : int =
-    let rec run (difference: int List) (lastNumbers: int List) =
+let getPrediction reports =
+    let rec run difference lastNumbers =
         if difference |> List.forall (fun i -> i = 0) then
             lastNumbers
         else
             run (getDifference difference) (lastNumbers @ [ difference[difference.Length - 1] ])
 
-    let lastNumbers = run reports []
-    lastNumbers |> List.sum
+    run reports [] |> List.sum
 
-let getStartPrediction (reports: int List) : int =
-    let rec run (difference: int List) (firstNumbers: int List) =
+let getStartPrediction reports =
+    let rec run difference firstNumbers =
         if difference |> List.forall (fun i -> i = 0) then
             firstNumbers
         else
@@ -39,12 +36,8 @@ let getStartPrediction (reports: int List) : int =
 
 
 
-let part1 (input: string List) =
-    let reports = getReports input
-    let predictions = reports |> List.map getPrediction
-    predictions |> List.sum |> string
+let part1 input =
+    getReports input |> List.map getPrediction |> List.sum |> string
 
-let part2 (input: string List) =
-    let reports = getReports input
-    let predictions = reports |> List.map getStartPrediction
-    predictions |> List.sum |> string
+let part2 input =
+    getReports input |> List.map getStartPrediction |> List.sum |> string
