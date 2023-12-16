faba60afa44d6e450bf84f4e1c8fdfa7fc5dba05
Chase Manning <chase@manning.dev>
2023-12-16 12:29:39 +0000

    minor code improvements

--- solutions/Day16.fs
+++ solutions/Day16.fs
@@ -4,91 +4,63 @@ open System
 
 type Mirrors = char List List
 
-type Direction =
-    | Up
-    | Down
-    | Left
-    | Right
+type Visit = { x: int; y: int; dir: char }
 
-type Visit =
-    { x: int; y: int; direction: Direction }
+let inRange x y (mirrors: Mirrors) =
+    x >= 0 && x < mirrors.[0].Length && y >= 0 && y < mirrors.Length
 
-let printMirrors (mirrors: Mirrors) (visited: Visit List) =
-    mirrors
-    |> List.mapi (fun y row ->
-        row
-        |> List.mapi (fun x c ->
-            if visited |> List.exists (fun v -> x = v.x && y = v.y) then
-                '#'
-            else
-                c))
-    |> List.map (fun s -> s |> List.map string |> String.concat "")
-    |> String.concat "\n"
-    |> printfn "%s"
-
-let getToVisit (mirrors: Mirrors) (visit: Visit) =
+let getToVisit mirrors visit =
     let x = visit.x
     let y = visit.y
-    let direction = visit.direction
+    let dir = visit.dir
 
-    if x < 0 || x >= mirrors.[0].Length || y < 0 || y >= mirrors.Length then
+    if not (inRange x y mirrors) then
         []
     else
         let current = mirrors.[y].[x]
 
         if current = '.' then
-            match direction with
-            | Up -> [ { x = x; y = y - 1; direction = Up } ]
-            | Down -> [ { x = x; y = y + 1; direction = Down } ]
-            | Left -> [ { x = x - 1; y = y; direction = Left } ]
-            | Right -> [ { x = x + 1; y = y; direction = Right } ]
+            match dir with
+            | 'U' -> [ { x = x; y = y - 1; dir = 'U' } ]
+            | 'D' -> [ { x = x; y = y + 1; dir = 'D' } ]
+            | 'L' -> [ { x = x - 1; y = y; dir = 'L' } ]
+            | _ -> [ { x = x + 1; y = y; dir = 'R' } ]
         else if current = '/' then
-            match direction with
-            | Up -> [ { x = x + 1; y = y; direction = Right } ]
-            | Down -> [ { x = x - 1; y = y; direction = Left } ]
-            | Left -> [ { x = x; y = y + 1; direction = Down } ]
-            | Right -> [ { x = x; y = y - 1; direction = Up } ]
+            match dir with
+            | 'U' -> [ { x = x + 1; y = y; dir = 'R' } ]
+            | 'D' -> [ { x = x - 1; y = y; dir = 'L' } ]
+            | 'L' -> [ { x = x; y = y + 1; dir = 'D' } ]
+            | _ -> [ { x = x; y = y - 1; dir = 'U' } ]
         else if current = '\\' then
-            match direction with
-            | Up -> [ { x = x - 1; y = y; direction = Left } ]
-            | Down -> [ { x = x + 1; y = y; direction = Right } ]
-            | Left -> [ { x = x; y = y - 1; direction = Up } ]
-            | Right -> [ { x = x; y = y + 1; direction = Down } ]
+            match dir with
+            | 'U' -> [ { x = x - 1; y = y; dir = 'L' } ]
+            | 'D' -> [ { x = x + 1; y = y; dir = 'R' } ]
+            | 'L' -> [ { x = x; y = y - 1; dir = 'U' } ]
+            | _ -> [ { x = x; y = y + 1; dir = 'D' } ]
         else if current = '-' then
-            match direction with
-            | Up ->
-                [ { x = x - 1; y = y; direction = Left }
-                  { x = x + 1; y = y; direction = Right } ]
-            | Down ->
-                [ { x = x - 1; y = y; direction = Left }
-                  { x = x + 1; y = y; direction = Right } ]
-            | Left -> [ { x = x - 1; y = y; direction = Left } ]
-            | Right -> [ { x = x + 1; y = y; direction = Right } ]
+            match dir with
+            | 'U' -> [ { x = x - 1; y = y; dir = 'L' }; { x = x + 1; y = y; dir = 'R' } ]
+            | 'D' -> [ { x = x - 1; y = y; dir = 'L' }; { x = x + 1; y = y; dir = 'R' } ]
+            | 'L' -> [ { x = x - 1; y = y; dir = 'L' } ]
+            | _ -> [ { x = x + 1; y = y; dir = 'R' } ]
         else if current = '|' then
-            match direction with
-            | Up -> [ { x = x; y = y - 1; direction = Up } ]
-            | Down -> [ { x = x; y = y + 1; direction = Down } ]
-            | Left -> [ { x = x; y = y - 1; direction = Up }; { x = x; y = y + 1; direction = Down } ]
-            | Right -> [ { x = x; y = y - 1; direction = Up }; { x = x; y = y + 1; direction = Down } ]
+            match dir with
+            | 'U' -> [ { x = x; y = y - 1; dir = 'U' } ]
+            | 'D' -> [ { x = x; y = y + 1; dir = 'D' } ]
+            | 'L' -> [ { x = x; y = y - 1; dir = 'U' }; { x = x; y = y + 1; dir = 'D' } ]
+            | _ -> [ { x = x; y = y - 1; dir = 'U' }; { x = x; y = y + 1; dir = 'D' } ]
         else
             raise (Exception "Unexpected token")
 
-let getVisits (mirrors: Mirrors) (entry: Visit) : Visit List =
-    let rec run (visited: Visit List) (toVisit: Visit List) : Visit List =
-        // printMirrors mirrors visited
-
+let getVisits mirrors entry =
+    let rec run visited (toVisit: Visit List) =
         if toVisit.Length = 0 then
             visited
         else
             let visit = toVisit.[toVisit.Length - 1]
             let newToVisit = toVisit |> List.rev |> List.tail |> List.rev
 
-            if
-                visit.x < 0
-                || visit.x >= mirrors.[0].Length
-                || visit.y < 0
-                || visit.y >= mirrors.Length
-            then
+            if not (inRange visit.x visit.y mirrors) then
                 run visited newToVisit
             else if visited |> List.exists (fun v -> v = visit) then
                 run visited newToVisit
@@ -97,32 +69,29 @@ let getVisits (mirrors: Mirrors) (entry: Visit) : Visit List =
 
     run [] [ entry ]
 
-let getEnergized (visited: Visit List) : int =
+let getEnergized visited =
     visited |> List.map (fun v -> (v.x, v.y)) |> List.distinct |> List.length
 
-let getMirrors (line: string List) : Mirrors =
+let getMirrors (line: string List) =
     line |> List.map (fun s -> s.ToCharArray() |> Array.toList)
 
-let possibleEntryVisits (mirrors: Mirrors) : Visit List =
+let possibleEntryVisits (mirrors: Mirrors) =
     let maxX = mirrors.[0].Length - 1
     let maxY = mirrors.Length - 1
 
-    let top = [ 0..maxX ] |> List.map (fun x -> { x = x; y = 0; direction = Down })
-    let bottom = [ 0..maxX ] |> List.map (fun x -> { x = x; y = maxY; direction = Up })
-    let left = [ 0..maxY ] |> List.map (fun y -> { x = 0; y = y; direction = Right })
-    let right = [ 0..maxY ] |> List.map (fun y -> { x = maxX; y = y; direction = Left })
+    let top = [ 0..maxX ] |> List.map (fun x -> { x = x; y = 0; dir = 'D' })
+    let bottom = [ 0..maxX ] |> List.map (fun x -> { x = x; y = maxY; dir = 'U' })
+    let left = [ 0..maxY ] |> List.map (fun y -> { x = 0; y = y; dir = 'R' })
+    let right = [ 0..maxY ] |> List.map (fun y -> { x = maxX; y = y; dir = 'L' })
 
     top @ bottom @ left @ right
 
-let part1 (input: string List) =
+let part1 input =
     let mirrors = getMirrors input
-    let visited = getVisits mirrors { x = 0; y = 0; direction = Right }
-    let energized = getEnergized visited
-    energized |> string
+    getVisits mirrors { x = 0; y = 0; dir = 'R' } |> getEnergized |> string
 
-let part2 (input: string List) =
+let part2 input =
     let mirrors = getMirrors input
     let entries = possibleEntryVisits mirrors
     let visits = entries |> List.map (fun e -> getVisits mirrors e)
-    let energized = visits |> List.map getEnergized |> List.max
-    energized |> string
+    visits |> List.map getEnergized |> List.max |> string
