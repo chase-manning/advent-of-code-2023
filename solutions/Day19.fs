0268e9da72a755d7c3566fc8a9a137ffba48c5f6
Chase Manning <chase@manning.dev>
2023-12-19 20:13:47 +0000

    add day 19 part 2

--- solutions/Day19.fs
+++ solutions/Day19.fs
@@ -20,6 +20,15 @@ type Workflow = { name: string; steps: Step List }
 
 type Part = { x: int; m: int; a: int; s: int }
 
+type PropertyRange = { min: int; max: int }
+
+type PartRange =
+    { value: string
+      x: PropertyRange
+      m: PropertyRange
+      a: PropertyRange
+      s: PropertyRange }
+
 let getStep (input: string) : Step =
     let parts = input.Split(":")
 
@@ -139,4 +148,213 @@ let part1 (input: string List) =
     let total = accepted |> List.map partTotal |> List.sum
     total |> string
 
-let part2 (input: string List) = "meow"
+let getPartRanges (workflows: Map<string, Workflow>) : PartRange List =
+    let rec run (partRange: PartRange) : PartRange List =
+        if partRange.value = "A" || partRange.value = "R" then
+            [ partRange ]
+        else
+            let workflow = workflows.[partRange.value]
+
+            let rec runSteps (index: int) (part: PartRange) : PartRange List =
+                let step = workflow.steps.[index]
+
+                if step.autoReturn then
+                    run
+                        { value = step.output
+                          x = part.x
+                          m = part.m
+                          a = part.a
+                          s = part.s }
+                else if step.greaterThan then
+                    match step.category with
+                    | X ->
+                        let matchingPartRangeInput: PartRange =
+                            { value = step.output
+                              x =
+                                { min = step.value + 1
+                                  max = part.x.max }
+                              m = { min = part.m.min; max = part.m.max }
+                              a = { min = part.a.min; max = part.a.max }
+                              s = { min = part.s.min; max = part.s.max } }
+
+                        let notMatchingPartRangeInput: PartRange =
+                            { value = step.output
+                              x = { min = part.x.min; max = step.value }
+                              m = { min = part.m.min; max = part.m.max }
+                              a = { min = part.a.min; max = part.a.max }
+                              s = { min = part.s.min; max = part.s.max } }
+
+                        let notMatchingPartRange = runSteps (index + 1) notMatchingPartRangeInput
+                        let matchingPartRange = run matchingPartRangeInput
+                        matchingPartRange @ notMatchingPartRange
+                    | M ->
+                        let matchingPartRangeInput: PartRange =
+                            { value = step.output
+                              x = { min = part.x.min; max = part.x.max }
+                              m =
+                                { min = step.value + 1
+                                  max = part.m.max }
+                              a = { min = part.a.min; max = part.a.max }
+                              s = { min = part.s.min; max = part.s.max } }
+
+                        let notMatchingPartRangeInput: PartRange =
+                            { value = step.output
+                              x = { min = part.x.min; max = part.x.max }
+                              m = { min = part.m.min; max = step.value }
+                              a = { min = part.a.min; max = part.a.max }
+                              s = { min = part.s.min; max = part.s.max } }
+
+                        let notMatchingPartRange = runSteps (index + 1) notMatchingPartRangeInput
+                        let matchingPartRange = run matchingPartRangeInput
+                        matchingPartRange @ notMatchingPartRange
+                    | A ->
+                        let matchingPartRangeInput: PartRange =
+                            { value = step.output
+                              x = { min = part.x.min; max = part.x.max }
+                              m = { min = part.m.min; max = part.m.max }
+                              a =
+                                { min = step.value + 1
+                                  max = part.a.max }
+                              s = { min = part.s.min; max = part.s.max } }
+
+                        let notMatchingPartRangeInput: PartRange =
+                            { value = step.output
+                              x = { min = part.x.min; max = part.x.max }
+                              m = { min = part.m.min; max = part.m.max }
+                              a = { min = part.a.min; max = step.value }
+                              s = { min = part.s.min; max = part.s.max } }
+
+                        let notMatchingPartRange = runSteps (index + 1) notMatchingPartRangeInput
+                        let matchingPartRange = run matchingPartRangeInput
+                        matchingPartRange @ notMatchingPartRange
+                    | S ->
+                        let matchingPartRangeInput: PartRange =
+                            { value = step.output
+                              x = { min = part.x.min; max = part.x.max }
+                              m = { min = part.m.min; max = part.m.max }
+                              a = { min = part.a.min; max = part.a.max }
+                              s =
+                                { min = step.value + 1
+                                  max = part.s.max } }
+
+                        let notMatchingPartRangeInput: PartRange =
+                            { value = step.output
+                              x = { min = part.x.min; max = part.x.max }
+                              m = { min = part.m.min; max = part.m.max }
+                              a = { min = part.a.min; max = part.a.max }
+                              s = { min = part.s.min; max = step.value } }
+
+                        let notMatchingPartRange = runSteps (index + 1) notMatchingPartRangeInput
+                        let matchingPartRange = run matchingPartRangeInput
+                        matchingPartRange @ notMatchingPartRange
+                else
+                    match step.category with
+                    | X ->
+                        let matchingPartRangeInput: PartRange =
+                            { value = step.output
+                              x =
+                                { min = part.x.min
+                                  max = step.value - 1 }
+                              m = { min = part.m.min; max = part.m.max }
+                              a = { min = part.a.min; max = part.a.max }
+                              s = { min = part.s.min; max = part.s.max } }
+
+                        let notMatchingPartRangeInput: PartRange =
+                            { value = step.output
+                              x = { min = step.value; max = part.x.max }
+                              m = { min = part.m.min; max = part.m.max }
+                              a = { min = part.a.min; max = part.a.max }
+                              s = { min = part.s.min; max = part.s.max } }
+
+                        let notMatchingPartRange = runSteps (index + 1) notMatchingPartRangeInput
+                        let matchingPartRange = run matchingPartRangeInput
+                        matchingPartRange @ notMatchingPartRange
+                    | M ->
+                        let matchingPartRangeInput: PartRange =
+                            { value = step.output
+                              x = { min = part.x.min; max = part.x.max }
+                              m =
+                                { min = part.m.min
+                                  max = step.value - 1 }
+                              a = { min = part.a.min; max = part.a.max }
+                              s = { min = part.s.min; max = part.s.max } }
+
+                        let notMatchingPartRangeInput: PartRange =
+                            { value = step.output
+                              x = { min = part.x.min; max = part.x.max }
+                              m = { min = step.value; max = part.m.max }
+                              a = { min = part.a.min; max = part.a.max }
+                              s = { min = part.s.min; max = part.s.max } }
+
+                        let notMatchingPartRange = runSteps (index + 1) notMatchingPartRangeInput
+                        let matchingPartRange = run matchingPartRangeInput
+                        matchingPartRange @ notMatchingPartRange
+                    | A ->
+                        let matchingPartRangeInput: PartRange =
+                            { value = step.output
+                              x = { min = part.x.min; max = part.x.max }
+                              m = { min = part.m.min; max = part.m.max }
+                              a =
+                                { min = part.a.min
+                                  max = step.value - 1 }
+                              s = { min = part.s.min; max = part.s.max } }
+
+                        let notMatchingPartRangeInput: PartRange =
+                            { value = step.output
+                              x = { min = part.x.min; max = part.x.max }
+                              m = { min = part.m.min; max = part.m.max }
+                              a = { min = step.value; max = part.a.max }
+                              s = { min = part.s.min; max = part.s.max } }
+
+                        let notMatchingPartRange = runSteps (index + 1) notMatchingPartRangeInput
+                        let matchingPartRange = run matchingPartRangeInput
+                        matchingPartRange @ notMatchingPartRange
+                    | S ->
+                        let matchingPartRangeInput: PartRange =
+                            { value = step.output
+                              x = { min = part.x.min; max = part.x.max }
+                              m = { min = part.m.min; max = part.m.max }
+                              a = { min = part.a.min; max = part.a.max }
+                              s =
+                                { min = part.s.min
+                                  max = step.value - 1 } }
+
+                        let notMatchingPartRangeInput: PartRange =
+                            { value = step.output
+                              x = { min = part.x.min; max = part.x.max }
+                              m = { min = part.m.min; max = part.m.max }
+                              a = { min = part.a.min; max = part.a.max }
+                              s = { min = step.value; max = part.s.max } }
+
+                        let notMatchingPartRange = runSteps (index + 1) notMatchingPartRangeInput
+                        let matchingPartRange = run matchingPartRangeInput
+                        matchingPartRange @ notMatchingPartRange
+
+            runSteps 0 partRange
+
+    let start =
+        { value = "in"
+          x = { min = 1; max = 4000 }
+          m = { min = 1; max = 4000 }
+          a = { min = 1; max = 4000 }
+          s = { min = 1; max = 4000 } }
+
+    run start
+
+let combinations (partRange: PartRange) : Int128 =
+    let x = partRange.x.max - partRange.x.min + 1
+    let m = partRange.m.max - partRange.m.min + 1
+    let a = partRange.a.max - partRange.a.min + 1
+    let s = partRange.s.max - partRange.s.min + 1
+
+    Int128.Parse(string x)
+    * Int128.Parse(string m)
+    * Int128.Parse(string a)
+    * Int128.Parse(string s)
+
+let part2 (input: string List) =
+    let partRanges = getPartRanges (getWorkflows input)
+    let accepted = partRanges |> List.filter (fun partRange -> partRange.value = "A")
+    printfn "%A" accepted
+
+    accepted |> List.map combinations |> List.sum |> string
