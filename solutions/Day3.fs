a5853c5e8301ce8c1a4cda4aa8c3c45141a7d598
Chase Manning <chase@manning.dev>
2023-12-28 21:53:12 +0000

    :art: day 3 code improvements

--- solutions/Day3.fs
+++ solutions/Day3.fs
@@ -10,79 +10,56 @@ type Number =
 
 let toInt c = System.Int32.Parse(c.ToString())
 
-let rec get_numbers
-    (input: string List)
-    (line: int)
-    (pos: int)
-    (start: int)
-    (value: int)
-    (numbers: Number List)
-    : Number List =
-    match line = input.Length with
-    | true -> numbers
-    | false ->
-        match pos = input[line].Length with
-        | true ->
-            get_numbers
-                input
-                (line + 1)
-                0
-                0
-                0
-                (match value with
-                 | 0 -> numbers
-                 | _ ->
-                     [ { line = line
-                         start = start
-                         value = value
-                         len = pos - start } ]
-                     @ numbers)
-        | false ->
-            match Char.IsNumber(input[line][pos]) with
-            | true -> (get_numbers input line (pos + 1) start (value * 10 + toInt (input[line][pos])) numbers)
-            | false ->
-                get_numbers
-                    input
-                    line
-                    (pos + 1)
-                    (pos + 1)
-                    0
-                    (match value with
-                     | 0 -> numbers
-                     | _ ->
-                         [ { line = line
-                             start = start
-                             value = value
-                             len = pos - start } ]
-                         @ numbers)
+let rec get_numbers (input: string List) : Number List =
+    let rec run (line: int) (pos: int) (start: int) (value: int) (numbers: Number List) =
+        let newNumbers =
+            ([ { line = line
+                 start = start
+                 value = value
+                 len = pos - start } ]
+             @ numbers)
+
+        if line = input.Length then
+            numbers
+        else if pos = input[line].Length then
+            if value = 0 then
+                run (line + 1) 0 0 0 numbers
+            else
+                run (line + 1) 0 0 0 newNumbers
+        else if Char.IsNumber(input[line][pos]) then
+            (run line (pos + 1) start (value * 10 + toInt (input[line][pos])) numbers)
+        else if value = 0 then
+            run line (pos + 1) (pos + 1) 0 numbers
+        else
+            run line (pos + 1) (pos + 1) 0 newNumbers
+
+    run 0 0 0 0 []
 
 let rec has_symbol (input: string List) (line: int) (pos: int) (len: int) : bool =
-    match line >= 0 && line < input.Length with
-    | false -> false
-    | true ->
-        match pos < 0 with
-        | true -> has_symbol input line (pos + 1) (len - 1)
-        | false ->
-            match pos = input[line].Length with
-            | true -> false
-            | false ->
-                match Char.IsNumber(input[line][pos]) || input[line][pos] = '.' with
-                | true ->
-                    match len <= 1 with
-                    | true -> false
-                    | false -> has_symbol input line (pos + 1) (len - 1)
-                | false -> true
+    if line >= 0 && line < input.Length then
+        if pos < 0 then
+            has_symbol input line (pos + 1) (len - 1)
+        else if pos = input[line].Length then
+            false
+        else if Char.IsNumber(input[line][pos]) || input[line][pos] = '.' then
+            if len <= 1 then
+                false
+            else
+                has_symbol input line (pos + 1) (len - 1)
+        else
+            true
+    else
+        false
 
 let rec get_asterisk (input: string List) (line: int) (pos: int) asterisks =
-    match line = input.Length with
-    | true -> asterisks
-    | false ->
-        match pos = input[line].Length with
-        | true -> get_asterisk input (line + 1) 0 asterisks
-        | false ->
-            match input[line][pos] = '*' with
-            | true -> get_asterisk input line (pos + 1) ((line, pos) :: asterisks)
-            | false -> get_asterisk input line (pos + 1) asterisks
+    if line = input.Length then
+        asterisks
+    else if pos = input[line].Length then
+        get_asterisk input (line + 1) 0 asterisks
+    else if input[line][pos] = '*' then
+        get_asterisk input line (pos + 1) ((line, pos) :: asterisks)
+    else
+        get_asterisk input line (pos + 1) asterisks
 
 
 let adjacent_numbers asterisk (numbers: Number List) =
@@ -95,7 +72,7 @@ let adjacent_numbers asterisk (numbers: Number List) =
 
 let get_gears (input: string List) =
     (get_asterisk input 0 0 [])
-    |> List.filter (fun asterisk -> (adjacent_numbers asterisk (get_numbers input 0 0 0 0 [])).Length = 2)
+    |> List.filter (fun asterisk -> (adjacent_numbers asterisk (get_numbers input)).Length = 2)
 
 let is_part (input: string List) (number: Number) : bool =
     has_symbol input (number.line - 1) (number.start - 1) (number.len + 2) // Checking above the number
@@ -104,7 +81,7 @@ let is_part (input: string List) (number: Number) : bool =
     || has_symbol input number.line (number.start + number.len) 1 // Checking right of the number
 
 let part1 (input: string List) =
-    (List.filter (is_part input) (get_numbers input 0 0 0 0 []))
+    (List.filter (is_part input) (get_numbers input))
     |> List.fold (fun acc number -> acc + number.value) 0
     |> string
 
@@ -114,5 +91,5 @@ let gear_ratio gear (numbers: Number List) : int =
 
 let part2 (input: string List) =
     get_gears input
-    |> List.fold (fun acc gear -> acc + (gear_ratio gear (get_numbers input 0 0 0 0 []))) 0
+    |> List.fold (fun acc gear -> acc + (gear_ratio gear (get_numbers input))) 0
     |> string
